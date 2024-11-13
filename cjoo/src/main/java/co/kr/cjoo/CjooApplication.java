61a25f2fae64fdec07c36c4a748d80dab5d9f1bb
yjooyoung <jooyoung3341@gmail.com>
2024-11-13 21:23:20 +0900

    aasd

---
+++ cjoo/src/main/java/co/kr/cjoo/CjooApplication.java
@@ -0,0 +1,13 @@
+package co.kr.cjoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CjooApplication.class, args);
+	}
+
+}
